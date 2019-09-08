cf4f8206749608c3c3c59675f14c6a36c560cbbb
Robert Busby <Robert.Wood.Busby@gmail.com>
2019-09-08 11:20:00 +0000

    Move config out of app into its own class

--- submission-api/src/main/java/com/kanjisoup/coffee/stain/submission/config/StainQueueConnectionFactoryConfig.java
+++ submission-api/src/main/java/com/kanjisoup/coffee/stain/submission/config/StainQueueConnectionFactoryConfig.java
@@ -2,7 +2,6 @@ package com.kanjisoup.coffee.stain.submission.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix="stain.queue")
 @Data
